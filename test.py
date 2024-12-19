f23d33c20ff3faaf79ca611f06e9aab1e4af19c5
richard-syKim <richard13431343@gmail.com>
2024-12-19 14:50:39 -0500

    Add test file

---
+++ test.py
@@ -0,0 +1,19 @@
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import spotipy
+import os
+
+load_dotenv("auth.env")
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-currently-playing"
+))
+
+print("Authentication successful!")
